862d0e39435cb5d8eecdbb4e0f64ce350fd3275b
vitaly.krasnov <Vitaly.Krasnov@x5.ru>
2024-03-09 17:46:24 +0300

    изменён алгоритм ввода значений элементов массива

--- ArrayFilter.cs
+++ ArrayFilter.cs
@@ -4,9 +4,16 @@ public class ArrayFilter
 {
     public void RunTask()
     {
-        Console.Write("Введите через пробел элементы массива: ");
-        string[] array = Console.ReadLine()!.Split(" ");
-        Console.Write(string.Join(" ", array));
+        Console.Write("Введите количество элементов массива: ");
+        int arrayLength = int.Parse(Console.ReadLine()!);
+        string[] array = new string[arrayLength];
+        for (int i = 0; i < arrayLength; i++)
+        {
+            Console.Write("Введите элемент массива и нажмите 'Enter' для ввода следующего: ");
+            array[i] = Console.ReadLine()!;
+        }
+        
+        Console.WriteLine($"[ {string.Join(", ", array)} ]");
     }
     
 }
